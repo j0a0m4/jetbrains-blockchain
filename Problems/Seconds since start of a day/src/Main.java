4a2c317d87ddb02a77f924524cbb22098dd8ef66
João Lopes <joaomarcoslopes@id.uff.br>
2020-09-13 21:02:17 -0300

    Seconds since start of a day

--- Problems/Seconds since start of a day/src/Main.java
+++ Problems/Seconds since start of a day/src/Main.java
@@ -1,5 +1,43 @@
+import java.time.LocalTime;
+import java.util.Optional;
+import java.util.Scanner;
+
+class MainIn {
+    public static Scanner scanner = new Scanner(System.in);
+    int number;
+
+    public MainIn() {
+        readNumber();
+    }
+
+    private void readNumber() {
+        this.number = scanner.nextInt();
+    }
+
+    public Optional<Integer> getNumber() {
+        return number <= 0
+                ? Optional.empty()
+                : Optional.of(number);
+    }
+}
+
+
 class Main {
+
+    private static String getSeconds(int number) {
+        return LocalTime
+                .of(0, 0, 0)
+                .plusSeconds(number)
+                .toString();
+    }
+
     public static void main(String[] args) {
-        // put your code here
+        var number = new MainIn()
+                .getNumber();
+
+        if (number.isPresent()) {
+            var result = getSeconds(number.get());
+            System.out.println(result);
+        }
     }
 }
